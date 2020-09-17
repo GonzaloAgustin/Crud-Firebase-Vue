1b3d1c4383b0c262aafc3330f74c31d8f523963b
Gonzalo DAlessandro <gDAlessandro@prismamp.com>
2020-09-17 12:23:29 -0300

    Boostrap mejoras

--- src/views/Inicio.vue
+++ src/views/Inicio.vue
@@ -7,6 +7,15 @@
                 <button class="btn btn-success m-3">Agregar Tarea</button>    
             </router-link>
             
+            <router-link to="/registrar" v-if="!validoUsuarioLogeado">
+                <button class="btn btn-info m-3">Registrar Usuario</button>    
+            </router-link>
+
+            <router-link to="/logIn" v-if="!validoUsuarioLogeado">
+                <button class="btn btn-primary m-3">Log In</button>    
+            </router-link>
+
+                <button class="btn btn-dark m-3" @click="logOut"  v-if="validoUsuarioLogeado">Log Out</button>  
 
             <div class="form-now">
                 <div class="form-group row justify-content-md-center">
@@ -21,37 +30,27 @@
                 </div>
             </div>
 
-            <router-link to="/registrar" v-if="!validoUsuarioLogeado">
-                <button class="btn btn-info m-3">Registrar Usuario</button>    
-            </router-link>
-
-            <router-link to="/logIn" v-if="!validoUsuarioLogeado">
-                <button class="btn btn-primary m-3">Log In</button>    
-            </router-link>
-
-                <button class="btn btn-dark m-3" @click="logOut"  v-if="validoUsuarioLogeado">Log Out</button>  
-
                 <div v-if="loading && validoUsuarioLogeado" class="text-center">
                     <h5 class="mt-3">Cargando contenido..</h5>
                     <div class="spinner-border text-success" role="status">
                     <span class="sr-only">Loading...</span>
                     </div>
                 </div>
+        </div>  
 
         <div v-if="validoUsuarioLogeado && !loading"> 
             <ul class="list-group">
-                <li v-for="(item, index) in buscadorGetters" :key="index" class="list-group-item">
-
+                <li v-for="(item, index) in buscadorGetters" :key="index" class="list-group-item justify-content-end">
+        
                     {{item.nombre}} - {{item.id}}
-
+        
                     <router-link :to="{name: 'Editar', params:{id: item.id}}">
-                        <button class="btn btn-sm btn-warning m-1 text-white">Editar</button>    
+                        <button class="btn btn-sm btn-warning m-1 px-3 text-white float-right">Editar</button>    
                      </router-link>
-                        <button class="btn btn-sm btn-danger" type="submit" @click="eliminarTarea(item.id)">Eliminar</button>
+                        <button class="btn btn-sm btn-danger m-1 float-right" type="submit" @click="eliminarTarea(item.id)">Eliminar</button>
                 </li>
             </ul>
-        </div>
-        </div>                   
+        </div>              
     </div>
 </template>
 
